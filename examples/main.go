be52bd4cb0ccb15d6fadc24fe1683887e35fdd02
hybfkuf <fufk863@gmail.com>
2023-03-12 13:42:03 +0800

    Add README.md

--- examples/main.go
+++ examples/main.go
@@ -10,10 +10,6 @@ import (
 )
 
 var (
-	////jmsServerURL = "http://oajumper.cowarobot.cn"
-	////accessKey    = "9ae9c9d7-4baf-41c0-b215-3ace8d91d783"
-	////secretKey    = "e97e87b2-e418-4018-8397-6c86eab1392c"
-
 	jmsServerURL = "http://10.250.100.40"
 	accessKey    = "cfa5d3d1-8fe1-496b-92c0-ff8e5024cff5"
 	secretKey    = "374c71eb-9a2c-4ec1-a35c-ec78a9c4a300"
