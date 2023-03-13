f880cb17de87918e10f9a7ec3750256fb852e5cc
hybfkuf <fufk863@gmail.com>
2023-03-13 23:08:16 +0800

    update Operator interface, add Logger, StructuredLogger interface

--- interface.go
+++ interface.go
@@ -4,17 +4,17 @@ package jumpserver
 // It has the ability to create, delete, update, list and get jumpserver objects.
 type Operator interface {
 	// Create creates a jumpserver object.
-	Create(Object) (Object, error)
+	Create(...[]Object) (Object, error)
 	// Delete deletes a jumpserver object.
 	Delete(Parameter) error
 	// Update updates jumpserver objects.
-	Update([]Object) ([]Object, error)
+	Update(...[]Object) ([]Object, error)
 	// UpdatePartial partial updates jumpserver objects.
-	UpdatePartial([]Object) ([]Object, error)
+	UpdatePartial(...[]Object) ([]Object, error)
 	// List lists all jumpserver objects
-	List() ([]Object, error)
+	List(Parameter) ([]Object, error)
 	// Get gets meet conditions jumpserver objects.
-	Get() ([]Object, error)
+	Get(id string) ([]Object, error)
 }
 
 //var _ = Operator((*AssetExecutor)(nil))
@@ -33,6 +33,25 @@ type Parameter interface {
 	GetID() string
 }
 
-type Logger interface{}
+type Logger interface {
+	Debug(args ...any)
+	Info(args ...any)
+	Warn(args ...any)
+	Error(args ...any)
+	Fatal(args ...any)
 
-type StructuredLogger interface{}
+	Debugf(format string, args ...any)
+	Infof(format string, args ...any)
+	Warnf(format string, args ...any)
+	Errorf(format string, args ...any)
+	Fatalf(format string, args ...any)
+}
+
+// StructuredLogger is structured logger interface used for user to custom themselves logger.
+type StructuredLogger interface {
+	Debugw(msg string, args ...any)
+	Infow(msg string, args ...any)
+	Warnw(msg string, args ...any)
+	Errorw(msg string, args ...any)
+	Fatalw(msg string, args ...any)
+}
